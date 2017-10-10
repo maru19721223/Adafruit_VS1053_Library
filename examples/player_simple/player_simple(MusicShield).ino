e0cc11a6622e795445617898cc511b2580cac713
maru19721223 <h-maru@yc4.so-net.ne.jp>
2017-10-11 00:43:33 +0900

    Update and rename player_simple.ino to player_simple(MusicShield).ino

--- examples/player_simple/player_simple(MusicShield).ino
+++ examples/player_simple/player_simple(MusicShield).ino
@@ -18,31 +18,31 @@
 #include <SD.h>
 
 // define the pins used
-//#define CLK 13       // SPI Clock, shared with SD card
-//#define MISO 12      // Input data, from VS1053/SD card
-//#define MOSI 11      // Output data, to VS1053/SD card
+#define CLK 13       // SPI Clock, shared with SD card
+#define MISO 12      // Input data, from VS1053/SD card
+#define MOSI 11      // Output data, to VS1053/SD card
 // Connect CLK, MISO and MOSI to hardware SPI pins. 
 // See http://arduino.cc/en/Reference/SPI "Connections"
 
 // These are the pins used for the breakout example
-#define BREAKOUT_RESET  9      // VS1053 reset pin (output)
-#define BREAKOUT_CS     10     // VS1053 chip select pin (output)
-#define BREAKOUT_DCS    8      // VS1053 Data/command select pin (output)
+//#define BREAKOUT_RESET  9      // VS1053 reset pin (output)
+//#define BREAKOUT_CS     10     // VS1053 chip select pin (output)
+//#define BREAKOUT_DCS    8      // VS1053 Data/command select pin (output)
 // These are the pins used for the music maker shield
-#define SHIELD_RESET  -1      // VS1053 reset pin (unused!)
-#define SHIELD_CS     7      // VS1053 chip select pin (output)
-#define SHIELD_DCS    6      // VS1053 Data/command select pin (output)
+#define SHIELD_RESET  8      // VS1053 reset pin (unused!)
+#define SHIELD_CS     6      // VS1053 chip select pin (output)
+#define SHIELD_DCS    7      // VS1053 Data/command select pin (output)
 
 // These are common pins between breakout and shield
-#define CARDCS 4     // Card chip select pin
+#define CARDCS 9     // Card chip select pin
 // DREQ should be an Int pin, see http://arduino.cc/en/Reference/attachInterrupt
-#define DREQ 3       // VS1053 Data request, ideally an Interrupt pin
+#define DREQ 2       // VS1053 Data request, ideally an Interrupt pin
 
 Adafruit_VS1053_FilePlayer musicPlayer = 
   // create breakout-example object!
-  Adafruit_VS1053_FilePlayer(BREAKOUT_RESET, BREAKOUT_CS, BREAKOUT_DCS, DREQ, CARDCS);
+//  Adafruit_VS1053_FilePlayer(BREAKOUT_RESET, BREAKOUT_CS, BREAKOUT_DCS, DREQ, CARDCS);
   // create shield-example object!
-  //Adafruit_VS1053_FilePlayer(SHIELD_RESET, SHIELD_CS, SHIELD_DCS, DREQ, CARDCS);
+  Adafruit_VS1053_FilePlayer(SHIELD_RESET, SHIELD_CS, SHIELD_DCS, DREQ, CARDCS);
   
 void setup() {
   Serial.begin(9600);
@@ -74,10 +74,9 @@ void setup() {
   
   // Play one file, don't return until complete
   Serial.println(F("Playing track 001"));
-  musicPlayer.playFullFile("track001.mp3");
+  musicPlayer.startPlayingFile("track001.mp3");
   // Play another file in the background, REQUIRES interrupts!
-  Serial.println(F("Playing track 002"));
-  musicPlayer.startPlayingFile("track002.mp3");
+
 }
 
 void loop() {
@@ -137,4 +136,3 @@ void printDirectory(File dir, int numTabs) {
      entry.close();
    }
 }
-
